995587528842a4e7f577a0c7dc8bde43d8d9b4fc
fernando19 <fernandoalbert19@gmail.com>
2024-10-22 10:59:13 -0400

    sizes

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,6 +5,10 @@
  */
 int main(void)
 {
+	int i;
+	double d;
+	char c;
+
 	printf("The sizes of an ints is: %lu.\n", (unsigned long)sizeof(i));
 	printf("The sizes if an double is: %lu. \n", (unsigned long)sizeof(d));
 	printf("The sizes of an c is: %lu. \n", (unsigned long)sizesof(c));
