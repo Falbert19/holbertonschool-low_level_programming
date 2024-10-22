1a5e43d6a1b31d8143309bc5f63e6455f0ed0626
fernando19 <fernandoalbert19@gmail.com>
2024-10-22 09:33:56 -0400

    pust betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-
-int main(int argc, char *argv[])
+/**
+ * main - print a strin
+ * Return: always 0
+ */
+int main(void)
 {
-	char str[] = "Programming is like building a multilingual puzzle\n";
+	char str[] = "\"Programming is like building a multilingual puzzle\n";
+
 	puts(str);
 
 	return (0);
