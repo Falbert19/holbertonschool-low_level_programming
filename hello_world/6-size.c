10b2ec10e83c39856a6afde9d721a02889c50cf3
fernando19 <fernandoalbert19@gmail.com>
2024-10-22 10:32:23 -0400

    char

---
+++ hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this will print various types on the comptr and run on
+ * Return: always 0
+ */
+int main(void)
+{
+	sprintf(char *str, sizes_tsiz e, const char *formart, ...);
+
+		return (0);
