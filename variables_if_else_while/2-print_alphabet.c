bd36256732326360531e55389af78de1c2dca367
fernando19 <fernandoalbert19@gmail.com>
2024-10-24 11:13:13 -0400

    alphalow

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - this will prnit the alphabet in lowercase
+ * Return: always (Success)
+ */
+
+int main(void)
+{
+	char c:
+	for (c = 'a'; c<= 'z'; c++)
+	putchar(c)
+	putchar('\n);
+	return (0);
+}
