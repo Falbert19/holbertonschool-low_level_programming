2ba7c1097a0265ca35edf4ade8d2d19a01b8de09
fernando19 <fernandoalbert19@gmail.com>
2024-10-27 22:54:57 -0400

    x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,22 @@
 #include "main.h"
 
 /**
- * main -check
- *
- * Return: Always 0.
+ * print_alphabet_x10 - make x10 alpha
+ * Return: void
  */
-int main(void)
-{
 
-	print_alphabet_x10();
-	return (0);
-}
+void print_alphabet_x10(void)
+
+{
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+
+		_putchar('\n');
+		i++;
+	}}}
