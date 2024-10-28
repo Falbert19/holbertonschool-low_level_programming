59c6d957d3500c2508f70f4d421c4727aa9c5d10
fernando19 <fernandoalbert19@gmail.com>
2024-10-27 21:30:23 -0400

    putloop

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print _putchar
+ * Return: Always 0 (Success0)
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
+
+
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this directory will hace nested loops examples and tasks
