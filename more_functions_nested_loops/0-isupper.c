757bd1ce61e26f8520d62297af44e84aefcfa7ae
fernando19 <fernandoalbert19@gmail.com>
2024-10-28 18:54:45 -0400

    changes to 0-isupper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -4,11 +4,14 @@
 #include <ctype.h>
 
 /**
- * int _isupper- checks uppercase
+ * _isupper - checks uppercase
+ * @x: ASCII value of the character to check
+ *
  * Return: 1 for upper letter or 0 for any else
  */
 
 int _isupper(int x)
+
 {
 	if (x >= 65 && x <= 90)
 	{
