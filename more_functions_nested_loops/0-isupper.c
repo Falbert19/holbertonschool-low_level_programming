b2c08e8c57b43462b3b38054329407968f8b542a
fernando19 <fernandoalbert19@gmail.com>
2024-10-28 18:40:19 -0400

    create files and code

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -4,10 +4,15 @@
 #include <ctype.h>
 
 /**
- * main - checks uppercase
- * Return: 
+ * int _isupper- checks uppercase
+ * Return: 1 for upper letter or 0 for any else
  */
 
-int _isupper(int c)
-
-
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
