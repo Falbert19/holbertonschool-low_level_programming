d55fa435c9e7e4a3098d6f1d2285dba5cb250506
fernando19 <fernandoalbert19@gmail.com>
2024-10-28 19:40:03 -0400

    multypli test

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - this funtions will multiply tow intergers
+ * @a: Number to multiply
+ * @b: Number to multiply with
+ * Return: The result of multiplication a * b
+ */
+
+int mul(int a, int b)
+
+{
+	return (a * b);
+}
