e70e4daa8f3d0e5ceedb70b8a28f42b5e0876ecd
fernando19 <fernandoalbert19@gmail.com>
2024-10-29 11:13:10 -0400

    most numbers task

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers from 0 to 9 except 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n != 2 && n != 4)
+		{
+		_putchar(n + '0');
+	}
+	}
+	_putchar('\n');
+}
+
+/**
+ * main - result
+ * Return: ALways 0
+ */
+
+int main(void)
+{
+	print_most_numbers();
+	return (0);
+}
