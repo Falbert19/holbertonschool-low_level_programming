981d17d65db88a42460762938fea939b0c2d40e9
fernando19 <fernandoalbert19@gmail.com>
2024-10-30 10:20:48 -0400

    create the files and funtions

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - this program will draw a straight line
+ * @n: ASCII value to _
+ * Return: void
+ */
+
+void print_line(int n)
+{
+
+	if (n <= 0)
+	{
+		_putchar(95);
+		_putchar('\n');
+	}
+}
