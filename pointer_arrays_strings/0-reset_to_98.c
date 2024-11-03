43fc5f8397fec08efc10391eb262472a4487b8bf
fernando19 <fernandoalbert19@gmail.com>
2024-11-03 17:20:46 -0400

    create of file readme, main.h, and task 0

---
+++ pointer_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - use a pointer to int and updates
+ * The value it points to to 98
+ *
+ * @n: An int intput pointer
+ *
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointer_arrays_strings/README.md
@@ -0,0 +1,1 @@
+In this file will have task and examples of pointers arrays and strings
