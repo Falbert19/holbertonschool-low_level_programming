6e9ac19028a8ecc131d8c80c59d2d8cec9470cee
fernando19 <fernandoalbert19@gmail.com>
2024-11-03 17:46:35 -0400

    creation adn compile of file strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * _strlen - Return length of string
+ * @str: string
+ * Return: The length of @str
+ */
+
+size_t _strlen(const char *str)
+
+{
+	size_t length = 0;
+
+	while (*str++)
+		length++;
+	return (length);
+}
