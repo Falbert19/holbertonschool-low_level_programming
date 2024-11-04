93a1e7ba6014dec6e5df4efd922ae33066926fc3
fernando19 <fernandoalbert19@gmail.com>
2024-11-03 23:12:32 -0400

    file changes

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,2 +1,20 @@
 #include "main.h"
 #include <stdio.h>
+
+/**
+ * reverse_array - rev the cintet of int
+ * @a: array of int rev
+ * @n: number
+ */
+
+void reverse_array(int *a, int n)
+{
+	int tmp, index;
+
+	for (index = n - 1; index >= n / 2; index--)
+	{
+	tmp = a[n - 1 - index];
+	a[n - 1 - index] = a[index];
+	a[index] = tmp;
+	}
+}
