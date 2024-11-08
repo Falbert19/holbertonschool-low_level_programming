8145978d36fabcd4d5f3ab8296ed4562ca39ff73
fernando19 <fernandoalbert19@gmail.com>
2024-11-08 17:40:02 -0400

    correction of file

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strpbrk - locates the first occu in string
+ * @s: string
+ * @accept: character
+ * Return: Always 0 (Success)
+ */
+
+char *_strpbrk(char *s, char *accept)
+
+{
+	int i, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	for (n = 0; accept[n] != '\0'; n++)
+	{
+		if (s[i] == accept[n])
+				return (s + i);
+	}
+	}
+	return (NULL);
+}
