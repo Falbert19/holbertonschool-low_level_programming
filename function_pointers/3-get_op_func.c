6ea273584453728d60bc3b6b98a88b62cce09e59
Falbert19 <fernandoalbert19@gmail.com>
2024-11-21 11:39:09 -0400

    file 3-getsfunct

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * get_op_func - the function to perform
+ * @s: operator string
+ * Return: a pointer to the function that corresponds to the operator
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (*(ops[i].op) == *s && s[1] == '\0')
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
