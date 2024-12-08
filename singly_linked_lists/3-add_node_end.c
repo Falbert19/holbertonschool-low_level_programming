9b7014abf66e7e96207b857420dfd331f0512f3c
Falbert19 <fernandoalbert19@gmail.com>
2024-12-08 18:05:28 -0400

    strlen delete

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -16,13 +16,21 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node;
 	list_t *current;
 	char *duplicated_str;
+	size_t len = 0;
+	size_t i = 0;
 
 	if (str == NULL)
 		return (NULL);
 
-	duplicated_str = strdup(str);
+	while (str[len] != '\0')
+		len++;
+
+	duplicated_str = malloc(len +1);
 	if (duplicated_str == NULL)
 		return (NULL);
+
+	for (i = 0; i <= len; i++)
+		duplicated_str[i] = str[i];
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
@@ -32,7 +40,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 
 	new_node->str = duplicated_str;
-	new_node->len = strlen(str);
+	new_node->len = len;
 	new_node->next = NULL;
 
 	if (*head == NULL)
