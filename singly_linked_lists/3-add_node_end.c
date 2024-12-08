1b48b19a03aafc34da796a08a9f03ba037930e6d
Falbert19 <fernandoalbert19@gmail.com>
2024-12-08 18:08:13 -0400

    betty corrected

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -25,37 +25,32 @@ list_t *add_node_end(list_t **head, const char *str)
 	while (str[len] != '\0')
 		len++;
 
-	duplicated_str = malloc(len +1);
+	duplicated_str = malloc(len + 1);
 	if (duplicated_str == NULL)
 		return (NULL);
 
 	for (i = 0; i <= len; i++)
 		duplicated_str[i] = str[i];
-
 	new_node = malloc(sizeof(list_t));
+
 	if (new_node == NULL)
 	{
 		free(duplicated_str);
 		return (NULL);
 	}
-
 	new_node->str = duplicated_str;
 	new_node->len = len;
 	new_node->next = NULL;
 
 	if (*head == NULL)
-
 	{
 		*head = new_node;
 		return (new_node);
 	}
-
 	current = *head;
 	while (current->next != NULL)
 		current = current->next;
-
-	current->next = new_node;
-
-	return (new_node);
+current->next = new_node;
+return (new_node);
 }
 
