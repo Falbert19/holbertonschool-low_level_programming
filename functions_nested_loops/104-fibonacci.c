97c068c5a88764c2f9afb0c33f4a661b4e08300d
Falbert19 <fernandoalbert19@gmail.com>
2024-12-27 02:41:12 -0400

    betty corrected

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - prints the first 98 Fibonacci numbers, starting with 1 and 2
- *
- * Return: Always 0 (Success)
+ * print_fibonacci - prints
  */
 
-int main(void)
+void print_fibonacci(void)
 {
 	unsigned long fib1 = 1, fib2 = 2;
 	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
@@ -18,37 +16,43 @@ int main(void)
 	for (count = 3; count <= 92; count++)
 	{
 		unsigned long next = fib1 + fib2;
+
 		printf(", %lu", next);
 		fib1 = fib2;
 		fib2 = next;
 	}
-
 
 	fib1_half1 = fib1 / 1000000000;
 	fib1_half2 = fib1 % 1000000000;
 	fib2_half1 = fib2 / 1000000000;
 	fib2_half2 = fib2 % 1000000000;
 
-	for (; count <= 98; count++)
+	for (count = 93; count <= 98; count++)
 	{
 		half1 = fib1_half1 + fib2_half1;
 		half2 = fib1_half2 + fib2_half2;
-
 		if (half2 >= 1000000000)
 		{
-			half1 += half2 / 1000000000;
+			half1 += 1;
 			half2 %= 1000000000;
 		}
-
 		printf(", %lu%09lu", half1, half2);
-
 		fib1_half1 = fib2_half1;
 		fib1_half2 = fib2_half2;
 		fib2_half1 = half1;
 		fib2_half2 = half2;
 	}
-
 	printf("\n");
+}
+
+/**
+ * main - return fibonacci print
+ * Return: always 0
+ */
+
+int main(void)
+{
+	print_fibonacci();
 	return (0);
 }
 
