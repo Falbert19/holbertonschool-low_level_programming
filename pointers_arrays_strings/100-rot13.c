6c532af9d745b0bc18d820bfe57a45d7e580b21b
Falbert19 <fernandoalbert19@gmail.com>
2024-12-28 17:13:01 -0400

    task 8

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @s: ``:Xinput string to be encoded
+ *
+ * Return: the encoded string
+ */
+
+char *rot13(char *s)
+{
+	char *ptr = s;
+	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i;
+
+	while (*ptr)
+	{
+		for (i = 0; alphabet[i] != '\0'; i++)
+		{
+			if (*ptr == alphabet[i])
+			{
+				*ptr = rot13[i];
+				break;
+			}
+		}
+		ptr++;
+	}
+	return s;
+}
+
