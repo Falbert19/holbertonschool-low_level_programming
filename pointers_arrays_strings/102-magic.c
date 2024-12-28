5848318ce62f81e5ffe50beb1095ddbaece3adff
Falbert19 <fernandoalbert19@gmail.com>
2024-12-28 17:25:34 -0400

    task 102

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -8,15 +8,20 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-
-
-
-
+	/*
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use the variable a
+	 * - you are not allowed to modify the variable p
+	 * - you can only write one statement
+	 * - you are not allowed to use ,
+	 * - you are not allowed to code anything else than the line of expected line of code at the expected line
+	 * - do not remove anything from the initial code (not even the comments)
+	 * - and don’t change anything but the line of code you are adding (don’t change the spaces to tabs!)
+	 * - you are allowed to use the standard library
+	 */
 	*(p + 5) = 98;
-
-
-
-
+	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
