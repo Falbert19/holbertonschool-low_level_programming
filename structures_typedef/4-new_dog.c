dcd98c92a38b05640b0810a78b0aa1fbfe57a90f
Falbert19 <fernandoalbert19@gmail.com>
2025-01-13 11:45:36 -0400

    redo task

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - Creates a new dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Return: Pointer to the new dog_t, or NULL if the function fails
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	char *name_copy, *owner_copy;
+
+	dog = malloc(sizeof(dog_t));
+	if (!dog)
+		return (NULL);
+
+	name_copy = malloc(strlen(name) + 1);
+	if (!name_copy) {
+		free(dog);
+		return (NULL);
+	}
+	strcpy(name_copy, name);
+
+	owner_copy = malloc(strlen(owner) + 1);
+	if (!owner_copy) {
+		free(name_copy);
+		free(dog);
+		return (NULL);
+	}
+	strcpy(owner_copy, owner);
+
+	dog->name = name_copy;
+	dog->age = age;
+	dog->owner = owner_copy;
+
+	return (dog);
+}
+
